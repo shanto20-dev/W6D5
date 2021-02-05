e13b0f8f30dcd96611d8ef95209b2bcde3875613
shanto20-dev <ecshanto@gmail.com>
2021-02-05 14:00:05 -0500

    Added class model and migrationfor Cats

---
+++ app/models/cat.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+require 'action_view'
+
+class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+    COLORS = ["Red", "Blue", "Yellow", "Pink", "Purple", "Black", "White"]
+    validates: :id, presence: true
+    validates: :birth_date, presence: true
+    validates: :color, presence: true, inclusion: {in: COLORS}
+    validates: :name, presence: true
+    validates: :sex, presence: true, length: {maximum: 1}, inclusion: {in: %w(M F)}
+    validates: :description, presence: true
+
+    def age
+
+    end
+end
