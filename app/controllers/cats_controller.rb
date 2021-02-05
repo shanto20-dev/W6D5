e5f21f57a55d1084437652c7651fdc2a35131ae7
shanto20-dev <ecshanto@gmail.com>
2021-02-05 15:40:24 -0500

    Added new form and create functionality

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,8 +8,26 @@ class CatsController < ApplicationController
     def show
         @cat = Cat.find(params[:id])
         render :show
-        
     end
 
+    def new 
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
+
+
+
+    private
+    def cat_params 
+        params.require(:cat).permit(:birth_date, :color, :sex, :name, :description)
+    end
 
 end
