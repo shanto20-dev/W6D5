1b1663f003b63396ec4b53dd5dcf95f922111fe4
Kenny Li <kenny.wj.li@gmail.com>
2021-02-05 16:16:41 -0500

    finished edit.html

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -22,8 +22,18 @@ class CatsController < ApplicationController
             render :new
         end
     end
-
-
+    def edit
+        @cat = Cat.find(params[:id])
+        render :edit 
+    end
+    def update
+        cat = Cat.find(params[:id])
+        if cat.update(cat_params) 
+            redirect_to cat_url(cat)
+        else
+            render :edit
+        end
+    end
 
     private
     def cat_params 
